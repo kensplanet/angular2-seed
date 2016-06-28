db2a82a0584ddfb15e86331d9e95852e2b6f7f39
kensplanet <kensplanet@yahoo.com>
2016-06-27 21:17:56 -0400

    Components

---
+++ dev/my-component.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-component',
+    template: `
+        This is my first component.
+    `
+})
+
+export class MyComponentComponent {
+
+}

cdd699f055d76276d84898221cc01acdb051ced2
kensplanet <kensplanet@yahoo.com>
2016-06-27 22:21:56 -0400

    Templates

--- dev/my-component.component.ts
+++ dev/my-component.component.ts
@@ -1,12 +1,19 @@
-import {Component} from 'angular2/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-component',
     template: `
-        This is my first component.
-    `
+        Hi, I am <span [style.color]="inputElement.value ==='yes' ? 'red' : ''">{{name}}</span> and this is my first Angular2 Component!
+         <span [class.is-awesome]="inputElement.value === 'yes'">It's so awesome !!!</span>
+         <br><br>
+         Is it awesome?
+         <input type="text" #inputElement (keyup)="0">
+         <br><br>
+         <button [disabled]="inputElement.value !=='yes'">Only enabled if 'yes' was entered</button>
+    `,
+    styleUrls: ['src/css/mycomponent.css']
 })
 
 export class MyComponentComponent {
-
+    name = "Ken";
 }
